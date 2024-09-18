abcdc63fa3d7b22666a56927d423c4e9b459a832
Mathias Benjaminsen <mabenjam@gmail.com>
2024-09-19 00:02:08 +0200

    Fix weekend vs weekday starts graph

--- src/components/graphs/EventsChartMidWeekVsWeekend.jsx
+++ src/components/graphs/EventsChartMidWeekVsWeekend.jsx
@@ -1,29 +1,18 @@
-import {
-  Card,
-  LineChart,
-  MultiSelect,
-  MultiSelectItem,
-  Select,
-  SelectItem,
-} from "@tremor/react";
+import { Card, LineChart, Select, SelectItem } from "@tremor/react";
 import { useEffect, useState } from "react";
 
 import { supabase } from "../../supabaseClient";
 import { Spinner } from "../Spinner";
 
-import {
-  getUniqueYears,
-  granularityLookup,
-  groupDataByGranularity,
-} from "../../helpers/chart";
+import { granularityLookup } from "../../helpers/chart";
 
 export function EventsChartMidWeekVsWeekend(props) {
   const { filter } = props;
 
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
-  const [granularity, setGranularity] = useState("week");
-  const [localFilter, setLocalFilter] = useState(["2024", "2019"]);
+  const [granularity, _setGranularity] = useState("year");
+  const [dataPoint, setDataPoint] = useState("events");
 
   useEffect(() => {
     setLoading(true);
@@ -31,7 +20,6 @@ export function EventsChartMidWeekVsWeekend(props) {
       const { data, error } = await supabase.rpc(
         "get_mid_week_and_weekend_starts",
         {
-          granularity,
           organisation_ids: filter.organisations,
         }
       );
@@ -45,11 +33,7 @@ export function EventsChartMidWeekVsWeekend(props) {
     };
 
     fetchData();
-  }, [granularity, filter]);
-
-  // Transform the data to group by year
-  const chartData = groupDataByGranularity(data, granularity);
-  const years = getUniqueYears(data);
+  }, [filter]);
 
   return (
     <Card
@@ -58,56 +42,49 @@ export function EventsChartMidWeekVsWeekend(props) {
       decorationColor="fuchsia"
     >
       <div className="flex justify-between items-start md:items-center md:flex-row flex-col mb-2 gap-2">
-        <h3 className="text-tremor-content-strong dark:text-dark-tremor-content-strong font-medium ">
-          Midt-uke vs helgestarter pr{" "}
-          {granularityLookup[granularity].toLowerCase()}
-        </h3>
-        <p className="text-tremor-content dark:text-dark-tremor-content mb-2">
-          Midt-uke = mandag til torsdag, helg = fredag til søndag.
-        </p>
+        <div className="flex flex-col">
+          <h3 className="text-tremor-content-strong dark:text-dark-tremor-content-strong font-medium ">
+            Midt-uke vs helge pr {granularityLookup[granularity].toLowerCase()}{" "}
+            (kretsløp og nærløp)
+          </h3>
+          <p className="text-tremor-content dark:text-dark-tremor-content mb-2">
+            Midt-uke = mandag til torsdag, helg = fredag til søndag.
+          </p>
+        </div>
 
         <div className="flex md:flex-row flex-col justify-between items-center gap-3">
-          {/* <Select
-            className="w-64"
-            defaultValue="number_of_events"
-            onValueChange={(value) => setDataPoint(value)}
-          >
-            <SelectItem value="number_of_events">Antal løp</SelectItem>
-            <SelectItem value="total_starts">Starter</SelectItem>
-          </Select> */}
           <Select
             className="w-64"
-            defaultValue="week"
+            defaultValue="events"
+            onValueChange={(value) => setDataPoint(value)}
+          >
+            <SelectItem value="events">Antal løp</SelectItem>
+            <SelectItem value="starts">Starter</SelectItem>
+          </Select>
+          {/* <Select
+            className="w-64"
+            defaultValue="year"
             onValueChange={(value) => setGranularity(value)}
           >
             <SelectItem value="week">Uke</SelectItem>
             <SelectItem value="month">Måned</SelectItem>
+            <SelectItem value="year">År</SelectItem>
           </Select>
 
-          <MultiSelect
-            className="w-64"
-            defaultValue={["2024", "2019"]}
-            onValueChange={(e) => setLocalFilter(e)}
-          >
-            {years
-              .sort((a, b) => b - a)
-              .map((year) => (
-                <MultiSelectItem value={year} key={`year-${year}`}>
-                  {year}
-                </MultiSelectItem>
-              ))}
-          </MultiSelect>
+           */}
         </div>
       </div>
       <div className="flex justify-center items-center h-80">
         {!loading ? (
           <LineChart
             className="h-80"
-            data={chartData}
+            data={data.map((item) => ({
+              period: item.period,
+              "Midt-uke": item[`weekday_${dataPoint}`],
+              Helg: item[`weekend_${dataPoint}`],
+            }))}
             index="period"
-            categories={localFilter
-              .map((year) => [`Midt-uke-${year}`, `Helg-${year}`])
-              .flat()}
+            categories={["Midt-uke", "Helg"]}
             colors={["teal", "yellow", "fuchsia", "lime"]}
             yAxisWidth={60}
             onValueChange={(v) => console.log(v)}
